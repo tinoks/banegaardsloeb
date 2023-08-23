eb4e3be737afa3e07b2f9da9f8f793eae6dbce5d
Tino Kastbjerg Stigsen <tinoks@gmail.com>
2023-08-23 13:13:00 +0200

    listening on any available interface

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ fastify.register(async (instance, opts, done) => {
 })
 
 
-fastify.listen({ port: 3000 }, function (err, address) {
+fastify.listen({ port: 3000,  host: '0.0.0.0'}, function (err, address) {
     if (err) {
         fastify.log.error(err)
         process.exit(1)
